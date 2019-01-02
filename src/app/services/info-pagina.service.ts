dc15bdd854a270d6814c6d2fa55f1d53eb6a7e7e
Gosick96 <arqcompu4@gmail.com>
2019-01-02 11:26:01 -0500

    Datos almacenados y mostrados en la pagina con FireBase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,15 +8,27 @@ import { InfoPagina } from '../interface/info-interface';
 export class InfoPaginaService {
 
   info: InfoPagina = {};
+  info_equipo: any = [];
   cargada: boolean = false;
 
   constructor( private http: HttpClient) { 
-    console.log('SERVICIO DE INFORMACION DE PAGINA');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+  private cargarInfo(){
     this.http.get('assets/data/data_pagina.json')
       .subscribe( (resp: InfoPagina) => {
         this.cargada = true;
         this.info = resp;
       });
+  }
+
+  private cargarEquipo() {
+    this.http.get('https://prueba-angular-a4bff.firebaseio.com/equipo.json')
+    .subscribe( resp => {
+      this.cargada = true;
+      this.info_equipo = resp;
+    });
   }
 }
